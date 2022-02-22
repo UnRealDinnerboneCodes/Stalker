5e097b52cf53ca6fa4ef20fad1ba31b29193a5ad
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-02-22 11:49:05 -0600

    Let Stalk

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM gradle:7.3.1-jdk17 as builder
+
+WORKDIR /build
+
+COPY build.gradle /build
+COPY gradle.properties /build
+COPY src /build/src
+COPY settings.gradle /build
+
+RUN gradle shadowJar
+
+FROM openjdk:17
+COPY --from=builder "/build/build/libs/Stakler-1.0.0+build.0-all.jar" Stakler-1.0.0-all.jar
+
+CMD ["java", "-jar", "Stakler-1.0.0-all.jar"]
---
+++ src/main/java/com/unrealdinnerbone/devicestalker/Stalker.java
@@ -0,0 +1,54 @@
+package com.unrealdinnerbone.devicestalker;
+
+import com.unrealdinnerbone.unreallib.discord.DiscordWebhook;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Stalker
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Stalker.class);
+    private static final List<String> BLACKLIST = new ArrayList<>();
+    private static final String WEBHOOK_URL = System.getenv("WEBHOOK_URL");
+
+    static {
+        BLACKLIST.add("rtl_433/LaCrosse-TX141THBv2/169");
+        BLACKLIST.add("rtl_433/Solight-TE44/34");
+    }
+
+    public static void main(String[] args) throws MqttException, InterruptedException {
+        LOGGER.info("Stalker started!");
+        String publisherId = UUID.randomUUID().toString();
+        IMqttClient client = new MqttClient("tcp://10.0.0.167:1885",publisherId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        client.connect(options);
+
+        CountDownLatch receivedSignal = new CountDownLatch(10);
+        client.subscribe("rtl_433/#", (topic, msg) -> {
+            String payload = new String(msg.getPayload());
+            if(!BLACKLIST.contains(topic)) {
+                DiscordWebhook.of(WEBHOOK_URL)
+                        .setUsername("Stalker")
+                        .setContent(StringEscapeUtils.escapeJson(payload))
+                        .execute();
+            }
+
+            LOGGER.info("Received {} message: {}", topic, new String(payload));
+            receivedSignal.countDown();
+        });
+        receivedSignal.await(1, TimeUnit.MINUTES);
+    }
+}
